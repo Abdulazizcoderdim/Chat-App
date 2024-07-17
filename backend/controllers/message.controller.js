ce73601c8255f8c143293be126e7418daf19d86f
AbdulazizRus <coderdim@gmail.com>
2024-07-17 20:19:49 +0500

    changes

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,3 +1,7 @@
 export const sendMessage = async (req, res) => {
-    console.log('first',req.params.id)
+  try {
+  } catch (error) {
+    console.log('Error in sendMessage controller:', error.message)
+    res.status(500).json({ error: 'Internal Server error' })
+  }
 }

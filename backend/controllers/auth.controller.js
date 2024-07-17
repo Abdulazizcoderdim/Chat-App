e0f9682b2ad2ae9ad10c946a33898608bf4ea2b6
AbdulazizRus <coderdim@gmail.com>
2024-07-17 19:38:43 +0500

    changes

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -78,5 +78,11 @@ export const login = async (req, res) => {
 }
 
 export const logout = (req, res) => {
-  console.log('Logout user')
+  try {
+    res.cookie('jwt', '', { maxAge: 0 })
+    res.status(200).json({ message: 'User logged out successfully' })
+  } catch (error) {
+    console.log('Error in Logout controller', error.message)
+    res.status(500).json({ error: 'Internal Server error' })
+  }
 }
